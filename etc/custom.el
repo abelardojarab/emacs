70f642f52c0e2bd38d7bf59a41160036fac2d656
mrbig <mrbig033@protonmail.com>
2019-06-18 20:12:05 -0300

    add toc to hydras

--- etc/custom.el
+++ etc/custom.el
@@ -3,14 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:foreground "red" :height 1.8))))
- '(show-paren-match ((t (:background "#292929" :foreground "sienna" :inverse-video nil :underline nil :slant normal :weight bold)))))
+ '(aw-leading-char-face ((t (:foreground "red" :height 1.8)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("/home/george/org/Agenda/agenda.org" "/home/george/org/Agenda/planning.org" "/home/george/org/Bahia_com_H/bahia.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/Arquitetura/arq_info.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/Arquitetura/arq_notes.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/Arquitetura/arq_tasks.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/Comunicacao/comunicacao.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/Desenvolvimento_Social/des_social.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/Gerencia_de_TI/gestao.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/POO/24/java.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/POO/poo.org" "/home/george/Studying/Unifacs/Segundo_Semestre/Matérias/POO/poo_notas.org"))
  '(package-selected-packages '(use-package))
- '(safe-local-variable-values '((blacken-mode))))
+ '(safe-local-variable-values '((eval sh-mode nil) (eval sh-mode t))))
