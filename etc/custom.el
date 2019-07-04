7515cf1bf0cf3eb0399e3143ac621f17da593d1f
mrbig <mrbig033@protonmail.com>
2019-07-04 03:00:08 -0300

    Scheduled commit

--- etc/custom.el
+++ etc/custom.el
@@ -9,10 +9,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(live-py-mode use-package)))
+ '(package-selected-packages '(highlight-indent-guides use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(show-paren-match ((t (:background "#292929" :foreground "dark orange" :inverse-video nil :underline nil :slant normal :weight bold)))))
+ )
