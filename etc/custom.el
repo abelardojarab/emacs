81c8a217c9e02b3f58361ad8557132d97d7b4d8a
mrbig <mrbig033@protonmail.com>
2019-07-14 18:00:10 -0300

    Scheduled commit

--- etc/custom.el
+++ etc/custom.el
@@ -9,9 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("/home/george/org/Agenda/agenda.org" "/home/george/org/Agenda/contatos.org" "/home/george/org/Agenda/leituras.org" "/home/george/org/Agenda/projetos.org" "/home/george/org/Agenda/saude.org" "/home/george/org/Agenda/writing.org"))
- '(package-selected-packages '(evil-multiedit use-package)))
+ '(caps-lock-mode nil)
+ '(package-selected-packages '(hacker-typer use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
